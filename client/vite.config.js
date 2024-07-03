63f23046cbc2206e2f4a24e8529fe7dadc2b7243
Prem Krishna <prem22@live.com>
2024-07-03 23:01:52 +0530

    Update code with latest changes

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  base: process.env.NODE_ENV === 'production' ? '/T---Shirt-Website/' : '/', // Dynamic base path
+  plugins: [react()]
+});
